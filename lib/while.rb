5c01b0278b5b73b3d7333c9bfd9c787f08858a0c
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-11 19:05:29 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,12 +2,9 @@ def using_while
   i= 0
 	levitation_force = 6
 	while levitation_force < 10
-	puts "Wingardium Leviosa"
-	levitation_force += 1
+  	puts "Wingardium Leviosa"
+  	levitation_force += 1
 	end
-	
-	
-
 end
 
 
